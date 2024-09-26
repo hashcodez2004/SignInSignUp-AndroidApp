f62ac469d7aa1a683f4f3853c7fceac571f0215d
Harsh Kumar Sharma <kumarsharmaharsh2004@gmail.com>
2024-09-26 22:08:34 +0530

    final changes done

--- app/src/main/java/com/hashdroid/loginsignup/MainActivity2.java
+++ app/src/main/java/com/hashdroid/loginsignup/MainActivity2.java
@@ -2,6 +2,7 @@ package com.hashdroid.loginsignup;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
@@ -9,11 +10,15 @@ import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,7 +28,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this, SystemBarStyle.auto(Color.WHITE, Color.WHITE));
         setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -31,8 +36,30 @@ public class MainActivity2 extends AppCompatActivity {
             return insets;
         });
 
-        TextView signUp = findViewById(R.id.login_txt_view4); // Make sure this ID exists in your XML layout
+        Window window = getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, android.R.color.white));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController insetsController = window.getInsetsController();
+            if (insetsController != null) {
+                insetsController.setSystemBarsAppearance(
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                        WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                );
+            }
+        } else {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+        TextView signUp = findViewById(R.id.login_txt_view4);
+
+        SpannableString spannableString = getSpannableString();
+
+        signUp.setText(spannableString);
+        signUp.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private @NonNull SpannableString getSpannableString() {
         SpannableString spannableString = new SpannableString("New here? Sign up");
 
         ClickableSpan clickableSpan = new ClickableSpan() {
@@ -51,8 +78,6 @@ public class MainActivity2 extends AppCompatActivity {
 
         // Set the clickable span for the text "Sign up"
         spannableString.setSpan(clickableSpan, 10, 17, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        signUp.setText(spannableString);
-        signUp.setMovementMethod(LinkMovementMethod.getInstance());
+        return spannableString;
     }
 }
